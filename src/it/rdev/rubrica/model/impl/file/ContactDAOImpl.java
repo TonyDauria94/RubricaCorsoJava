9340b021415fd4cc4622fd0669e738eb82147a02
TonyDauria94 <TonyDauria94@gmail.com>
2021-06-02 01:19:32 +0200

    Implementato metodo getAll().

--- src/it/rdev/rubrica/model/impl/file/ContactDAOImpl.java
+++ src/it/rdev/rubrica/model/impl/file/ContactDAOImpl.java
@@ -1,10 +1,14 @@
 package it.rdev.rubrica.model.impl.file;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import it.rdev.rubrica.model.Contact;
@@ -15,7 +19,28 @@ public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
 
 	@Override
 	public List<Contact> getAll() {
-		return null;
+		
+		List<Contact> list = new ArrayList<>();
+		
+		DataSource ds = DataSource.getInstance();
+		File f = ds.getFile();
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+		    String line;
+		    int i = 0;
+		    while ((line = br.readLine()) != null) {
+		    	list.add(ContactSerializer.deserialize(line, i++));
+		    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return list;
 	}
 	
 	@Override
