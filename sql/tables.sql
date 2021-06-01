3487868c0355d9bc428f87656bb28d6d453ef39f
TonyDauria94 <tonydauria94@gmail.com>
2021-06-01 13:13:28 +0200

    Prima implementazione DAO mysql.

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,6 +1,42 @@
+CREATE DATABASE `rubrica`;
+USE `rubrica`;
+
 CREATE TABLE `contacts` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(25) DEFAULT NULL,
   `surname` varchar(25) DEFAULT NULL,
+  
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `emails` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `contact_id` bigint UNSIGNED,
+  `email` varchar(40),
+  
+  UNIQUE (`contact_id`, `email`),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `phone_numbers` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `contact_id` bigint UNSIGNED,
+  `phone` varchar(20),
+  
+  UNIQUE (`contact_id`, `phone`),
+  PRIMARY KEY (`id`)
+);
+
+
+/* AGGIUNGO LE FOREIGN KEY */
+ALTER TABLE `emails`
+ADD CONSTRAINT `fk_emails_contacts` 
+	FOREIGN KEY (`contact_id`) 
+	REFERENCES `contacts`(`id`)
+	ON DELETE CASCADE;
+	
+ALTER TABLE `phone_numbers`
+ADD CONSTRAINT `fk_phone_numbers_contacts` 
+	FOREIGN KEY (`contact_id`) 
+	REFERENCES `contacts`(`id`)
+	ON DELETE CASCADE;
--- src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
+++ src/it/rdev/rubrica/model/impl/rdbms/ContactDAOImpl.java
@@ -9,13 +9,15 @@ import it.rdev.rubrica.model.Contact;
 import it.rdev.rubrica.model.ContactDAO;
 
 public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
-	
-	private final String TABLE_NAME = "contacts";
+
+	private final String TABLE_CONTACTS = "contacts";
+	private final String TABLE_EMAILS = "emails";
+	private final String TABLE_PHONES = "phone_numbers";
 
 	public List<Contact> getAll() {
 		List<Contact> contacts = new ArrayList<>();
 		try {
-			ResultSet rs = this.executeQuery("SELECT id, name, surname FROM " + TABLE_NAME);
+			ResultSet rs = this.executeQuery("SELECT id, name, surname FROM " + TABLE_CONTACTS);
 			while(rs.next()) {
 				contacts.add(
 						new Contact()
@@ -28,20 +30,64 @@ public class ContactDAOImpl extends AbstractDAO<Contact> implements ContactDAO {
 		}
 		return contacts;
 	}
+	
 
 	@Override
-	public boolean persist(Contact o) throws SQLException {
-		return false;
+	public boolean persist(Contact t) throws SQLException {
+
+		// Recupero l'id della insert in modo che si possa procedere con l'inserimento anche
+		// delle email e dei numeri di telefono.
+		Long id = this.executeInsert("INSERT INTO " + TABLE_CONTACTS + "(name, surname)" + 
+							"VALUES (?, ?);", t.getName(), t.getSurname() );
+		
+		for (String phone : t.getPhoneNumbers()) {
+			this.executeInsert("INSERT INTO " + TABLE_PHONES + "(contact_id, phone)" + 
+					"VALUES (?, ?);", id, phone );
+		}
+		
+		for (String email : t.getEmails()) {
+			this.executeInsert("INSERT INTO " + TABLE_EMAILS + "(contact_id, email)" + 
+					"VALUES (?, ?);", id, email );
+		}
+		
+		return id != null;
+		
 	}
 
 	@Override
 	public boolean delete(Contact t) throws SQLException {
-		return false;
+		return this.executeUpdate("DELETE FROM " + TABLE_CONTACTS + " WHERE id=?;", t.getId()) > 0;
+		
 	}
 
 	@Override
 	public boolean update(Contact t) throws SQLException {
-		return false;
+		
+		// Se l'id non è impostato, è inutile proseguire con l'esecuzione
+		if (t.getId() == null)
+			return false;
+		
+		this.executeUpdate("UPDATE " + TABLE_CONTACTS
+							+ " SET name = ?, surname = ?"
+							+ " WHERE id=?;", t.getName(), t.getSurname(), t.getId());
+		
+		// elimino dal db tutte le email e i telefoni per poi reinserirli.
+		// TODO meccanismo da migliorare...
+		this.executeUpdate("DELETE FROM " + TABLE_EMAILS + " WHERE contact_id=?", t.getId());
+
+		this.executeUpdate("DELETE FROM " + TABLE_PHONES + " WHERE contact_id=?", t.getId());
+		
+		for (String phone : t.getPhoneNumbers()) {
+			this.executeInsert("INSERT INTO " + TABLE_PHONES + "(contact_id, phone)" + 
+					"VALUES (?, ?);", t.getId(), phone );
+		}
+		
+		for (String email : t.getEmails()) {
+			this.executeInsert("INSERT INTO " + TABLE_EMAILS + "(contact_id, email)" + 
+					"VALUES (?, ?);", t.getId(), email );
+		}
+		
+		return true;
 	}
 
 }
